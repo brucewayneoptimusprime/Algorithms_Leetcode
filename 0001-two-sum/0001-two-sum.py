af3fc5379a1c2787d6e41f2d2da338a2f078c245
ADITYA VIKRAM SINGH <adityapersonalusefighttry@gmail.com>
2026-09-06 13:41:20 +0530

    Time: 3 ms (53.6%), Space: 20.4 MB (58.9%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(0, len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
+        hashmap_based_dict_lol =  {}
+
+        for current_index, current_value in enumerate(nums):
+            if target - nums[current_index] not in hashmap_based_dict_lol:
+                hashmap_based_dict_lol[current_value] = current_index
+            elif target - nums[current_index] in hashmap_based_dict_lol:
+                return [hashmap_based_dict_lol[target - nums[current_index]], current_index]
+
         
